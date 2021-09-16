df5e40d9842199c48697de53c1f8409c3d5ef1a3
unknown <manuel.rodriguez.ch1@gmail.com>
2021-09-15 21:59:27 -0300

    se agregó objeto dino sumo y botones nuevos

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,4 +2,6 @@
 $(function () {
   $("#DinoBoxerButton").click(() => $("#container").fadeIn());
   $("#DinoBoxerButton2").click(() => $("#container").fadeOut());
+  $("#DinoSumoButton").click(() => $("#container").fadeIn());
+  $("#DinoSumoButton2").click(() => $("#container").fadeOut());
 });
--- scripts/dinowarrior.js
+++ scripts/dinowarrior.js
@@ -18,18 +18,40 @@ let dinoBoxer = new dinoWarrior(
   "35",
   "45000 kisaurus"
 );
+let dinoSumo = new dinoWarrior(
+  "Yokoceratops",
+  "Desert Sumo",
+  "Male",
+  "50",
+  "80000 kisaurus"
+);
 
 $(document).ready(function () {
   console.log(dinoBoxer.character);
+
   $("#DinoBoxerButton").on("click", function () {
     $("#dinoBoxerStats").append(
       `<img src="./images/dino-boxer.png" width="300px" alt=""/>
       <div>
-      <h2>Name: ${dinoBoxer.character.name}</h2>
+      <h3>Name: ${dinoBoxer.character.name}</h3>
       <h2>fight Style: ${dinoBoxer.character.fightStyle}</h2>
       <h2>Genetic: ${dinoBoxer.character.genetic}</h2>
       <h2>Age:${dinoBoxer.character.age}</h2>
-      <h2>${dinoBoxer.character.strength}</h2>
+      <h2>Strength: ${dinoBoxer.character.strength}</h2>
+      </div>
+      `
+    );
+  });
+
+  $("#DinoSumoButton").on("click", function () {
+    $("#dinoSumoStats").append(
+      `<img src="./images/dino-sumo.png" width="300px" alt=""/>
+      <div>
+      <h3>Name: ${dinoSumo.character.name}</h3>
+      <h2>fight Style: ${dinoSumo.character.fightStyle}</h2>
+      <h2>Genetic: ${dinoSumo.character.genetic}</h2>
+      <h2>Age:${dinoSumo.character.age}</h2>
+      <h2>Strength: ${dinoSumo.character.strength}</h2>
       </div>
       `
     );
